2562c582f7ed363c9f11cc37b72c9d435f2744ce
olviia <olviia.stroivans@gmail.com>
2023-09-11 13:09:37 +0100

    Changed movement to Free Look Camera

---
+++ CubicAdventures/Assets/Scripts/ThirdPersonMovement.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:18747a380bef12ae55e1990c919127a64a173c9e00e88acc991d7fb0ab49c7af
+size 1388
---
+++ CubicAdventures/ReadMe.txt
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:2c379014fc8399b4aa9f7f1c3cab65f7d079502eb6a0633a390aacfcae33d2f7
+size 36
