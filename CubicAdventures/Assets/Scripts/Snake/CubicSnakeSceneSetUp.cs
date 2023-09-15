2c7a151cbcfc04646c8e96d87c9d43816b765571
olviia <olviia.stroivans@gmail.com>
2023-09-15 22:08:13 +0100

    generated field for snake, generate one body element

--- CubicAdventures/Assets/Scripts/Snake/CubicSnakeSceneSetUp.cs
+++ CubicAdventures/Assets/Scripts/Snake/CubicSnakeSceneSetUp.cs
@@ -7,29 +7,82 @@ public class CubicSnakeSceneSetUp : MonoBehaviour {
     [SerializeField] private Camera myCamera;
     [SerializeField] private GameObject player;
     [SerializeField] private GameObject upperController;
-    [SerializeField] private float cubeSize = 0.05f;
+    [SerializeField] private GameObject[] walls;
+    [SerializeField] private float wallsMargins = 30;
+
+    private float pixelsInUnit;
+    private static Vector3[] fieldInUnit;
 
 
 
     // Start is called before the first frame update
-    void Start()
-    {
-          float screenWidth = Screen.width;
-          float screenHeight = Screen.height;
+    void Start() {
+        float screenWidth = Screen.width;
+        float screenHeight = Screen.height;
 
 
         Vector3[] upperControllerCorners = new Vector3[4];
         upperController.GetComponent<RectTransform>().GetWorldCorners(upperControllerCorners);
         Vector3 cubeCenter = player.GetComponent<Renderer>().bounds.center;
 
-        float fieldHeight = screenHeight - upperControllerCorners[0].y;
-        float desiredCubeSize = cubeSize * fieldHeight;
+        float controllerHeight = upperControllerCorners[1].y;
+        float fieldHeight = screenHeight - controllerHeight;
 
         myCamera.transform.rotation = Quaternion.Euler(90, 0, 0);
-        myCamera.transform.position = new Vector3(cubeCenter.x, 30f, cubeCenter.z);
-        //myCamera.GetComponent<Transform>.rotation
-          PositionCamera();
-        
+
+        //upper and down wall length is screenWidth - wallsMargins*2 (x coordinate)
+        //left and right wall length is fieldHeight - wallsMargins*2 (y coordinate)
+
+        //define how many pixels are in one unit
+        Vector3 wallMin = walls[0].GetComponent<Renderer>().bounds.min;
+        Vector3 wallMax = walls[0].GetComponent<Renderer>().bounds.max;
+        Vector3 toScreenMin = myCamera.WorldToScreenPoint(wallMin);
+        Vector3 toScreenMax = myCamera.WorldToScreenPoint(wallMax);
+
+        //how many pixels are in 1 unit measurement
+        //x and y are the same and equal 1 if someone has't changed the scene
+
+        pixelsInUnit = toScreenMax.x - toScreenMin.x;
+
+        myCamera.transform.position = new Vector3(cubeCenter.x, 30f, cubeCenter.z-controllerHeight/(2*pixelsInUnit));
+
+
+
+
+        float targetWallWidth = screenWidth - wallsMargins * 2;
+        float targetWallHeight = fieldHeight - wallsMargins * 2;
+
+        float multiplierX = targetWallWidth / pixelsInUnit;
+        float multiplierY = targetWallHeight / pixelsInUnit;
+
+        walls[0].transform.localScale = new Vector3(multiplierX, 1f, 1f);
+        walls[1].transform.localScale = new Vector3(multiplierX, 1f, 1f);
+        walls[2].transform.localScale = new Vector3(multiplierY, 1f, 1f);
+        walls[3].transform.localScale = new Vector3(multiplierY, 1f, 1f);
+
+        //how many units in magrin
+        float marginVertical = (fieldHeight/2 - wallsMargins - pixelsInUnit/2) / pixelsInUnit;
+        float marginHorizontal = (screenWidth/2 - wallsMargins- pixelsInUnit/2) / pixelsInUnit;
+
+
+        walls[0].transform.position = new Vector3(0f, 0f, marginVertical);
+        walls[1].transform.position = new Vector3(0f, 0f, -marginVertical);
+        walls[2].transform.position = new Vector3(-marginHorizontal, 0f, 0f);
+        walls[3].transform.position = new Vector3(marginHorizontal, 0f, 0f);
+
+        //assign values of world coordinates of fields corners
+        fieldInUnit = new Vector3[4];
+        fieldInUnit[0] = new Vector3(-(screenWidth / 2 - wallsMargins - pixelsInUnit) / pixelsInUnit, 0f, (fieldHeight / 2 - wallsMargins - pixelsInUnit) / pixelsInUnit);
+        fieldInUnit[1] = new Vector3((screenWidth / 2 - wallsMargins - pixelsInUnit) / pixelsInUnit, 0f, (fieldHeight / 2 - wallsMargins - pixelsInUnit) / pixelsInUnit);
+        fieldInUnit[2] = new Vector3((screenWidth / 2 - wallsMargins - pixelsInUnit) / pixelsInUnit, 0f, -(fieldHeight / 2 - wallsMargins - pixelsInUnit) / pixelsInUnit);
+        fieldInUnit[3] = new Vector3(-(screenWidth / 2 - wallsMargins - pixelsInUnit) / pixelsInUnit, 0f, -(fieldHeight / 2 - wallsMargins - pixelsInUnit) / pixelsInUnit);
+
+        Debug.Log(fieldInUnit[0]);
+        Debug.Log(fieldInUnit[1]);
+        Debug.Log(fieldInUnit[2]);
+        Debug.Log(fieldInUnit[3]);
+
+
     }
 
     // Update is called once per frame
@@ -38,7 +91,12 @@ public class CubicSnakeSceneSetUp : MonoBehaviour {
         
     }
 
-    private void PositionCamera() { 
-
+    public  float getPixelsInOneUnit() {
+        return pixelsInUnit;
     }
+    public static Vector3[] getFieldInUnits() {
+        return fieldInUnit;
+    }
+
+
 }
---
+++ CubicAdventures/Assets/Scripts/Snake/SnakeLogic.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class SnakeLogic : MonoBehaviour
+{
+
+    [SerializeField] private GameObject player;
+    [SerializeField] private GameObject element; 
+    [SerializeField] private TextMeshProUGUI scoreObj;
+
+    private static float score;
+    private void Start() {
+        scoreObj.GetComponent<TextMeshProUGUI>().SetText("" + score);
+    }
+
+    private bool noElements = true;
+    public static bool isGameFinished = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (noElements) {
+            generateElement();
+        }
+        
+    }
+
+    public void generateElement() {
+        Vector3[] field = CubicSnakeSceneSetUp.getFieldInUnits();
+
+        float x = Random.Range(field[0].x, field[1].x);
+        float z = Random.Range(field[2].z, field[1].z);
+        Debug.Log(Physics.CheckSphere(new Vector3(x, 0.8f, z), 0.6f));
+
+        if (!(Physics.CheckSphere(new Vector3(x, 0.8f, z), 0.6f))) {
+            Instantiate(element, new Vector3(x, 0f, z), Quaternion.identity);
+            noElements = false;
+        } else {
+            generateElement();
+        }
+
+
+    }
+    public void onElementCollide() {
+        noElements = true;
+        score++;
+
+    }
+
+}
